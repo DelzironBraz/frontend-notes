a15115b6a724172b3fe765c8609ce9e975d29459
Delziron Braz <44593154+DelzironBraz@users.noreply.github.com>
2023-12-27 15:34:53 -0300

    Style/better styles (#1)
    * style: add new breakpoints and styles
    
    * style: change text for no notes

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import './App.css';
 import React, { useEffect, useState } from 'react';
 
-import { Box, Button, Card, CardActions, CardContent, Grid, TextField } from '@mui/material';
+import { Box, Button, Card, CardActions, CardContent, Grid, TextField, AppBar, Toolbar } from '@mui/material';
 import Typography from '@mui/material/Typography';
 
 import { Note } from './interface/note';
@@ -13,6 +13,7 @@ import { addNewNote, deleteNote, getAllNotes, updateNotes } from './utils/fetchF
 
 const App = () => {
   const [notes, setNotes] = useState<Note[]>([]);
+
   const [title, setTitle] = useState('');
   const [content, setContent] = useState('');
   const [selectedNote, setSelectedNote] = useState<Note | null>(null);
@@ -80,9 +81,18 @@ const App = () => {
 
   return (
     <Box>
-      <Grid container spacing={{ xs: 2, md: 3 }} columns={{ sm: 8, md: 16 }}>
-        <Grid item sm={8} md={4}>
-          <Box component='form' sx={{ display: 'flex', flexDirection: 'column', justifyContent: 'space-between', alignItems: 'center', gap: '1rem' }} onSubmit={(event) => selectedNote ? handleUpdateNote(event) : handleAddNote(event)}>
+      <Box sx={{ marginBottom: '5rem' }}>
+        <AppBar>
+          <Toolbar>
+            <Typography variant="h6" component="div">
+              Notes.
+            </Typography>
+          </Toolbar>
+        </AppBar>
+      </Box>
+      <Grid container spacing={{ xs: 2, md: 3 }} columns={{ sm: 8, md: 16 }} sx={{ justifyContent: 'center' }}>
+        <Grid item sm={8} md={4} sx={{ width: '100%' }}>
+          <Box component='form' sx={{ display: 'flex', flexDirection: 'column', justifyContent: 'space-between', alignItems: 'center', gap: '1rem', paddingLeft: '1rem', paddingRight: '1rem' }} onSubmit={(event) => selectedNote ? handleUpdateNote(event) : handleAddNote(event)}>
             <TextField
               id="outlined-basic"
               label="Title"
@@ -90,16 +100,18 @@ const App = () => {
               value={title}
               required
               onChange={(event) => setTitle(event.target.value)}
+              sx={{ width: '100%' }}
             />
             <TextField
               id="filled-multiline-flexible"
               label="Content"
               multiline
-              rows={10}
+              rows={4}
               variant="outlined"
               value={content}
               required
               onChange={(event) => setContent(event.target.value)}
+              fullWidth
             />
             {
               selectedNote ?
@@ -115,7 +127,7 @@ const App = () => {
           </Box>
         </Grid>
         <Grid item sm={8} md={12}>
-          <Grid container spacing={{ xs: 2, md: 3 }} columns={{ sm: 4, md: 12 }}>
+          <Grid container spacing={4} columns={{ xs: 4, sm: 8, md: 12 }} sx={{ justifyContent: 'center', alignItems: 'center', paddingRight: '1rem', paddingLeft: '1rem' }}>
             {
               notes.length ? (
                 notes.map((note) => (
@@ -137,8 +149,8 @@ const App = () => {
                 ))
 
               ) : (
-                <Grid item sm={4} md={4}>
-                  <h1>No notes to show</h1>
+                <Grid item sm={12} sx={{ width: '100%', textAlign: 'center' }}>
+                  <Typography variant='h4' >No notes to show</Typography>
                 </Grid>
               )
             }
